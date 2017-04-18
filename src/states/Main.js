6a39d9929ef2841a0831702d2ebfc02737a695c1
Prithivi Raj G <prithiviraj1991@gmail.com>
2017-04-18 19:26:04 +0530

    latest changes

--- src/states/Main.js
+++ src/states/Main.js
@@ -5,18 +5,31 @@ class Main extends Phaser.State {
 	create() {
 
 		//Enable Arcade Physics
-		this.game.physics.startSystem(Phaser.Physics.ARCADE);
+		//this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
 		//Set the games background colour
 		this.game.stage.backgroundColor = '#cecece';
 
 		//Example of including an object
 		//let exampleObject = new ExampleObject(this.game);
+
+		var word = "test"
+		var me = this;
+		new Cache(this.game);
+		if(me.game.cache.getText('dictionary').indexOf(' ' + word + ' ') > -1){
+			alert("exists");
+		} else {
+			alert("does not exist");
+		}
+
+		
 	}
 
 	update() {
 		
 	}
+
+	
 
 }
 
--- src/states/Preload.js
+++ src/states/Preload.js
@@ -10,6 +10,25 @@ class Preload extends Phaser.State {
 	create() {
 		//NOTE: Change to GameTitle if required
 		this.game.state.start("Main");
+
+		 this.game.load.text('dictionary', 'assets/dictionary.txt');
+		 
+		 console.log("dictionary")
+	}
+
+	init(){
+        var me = this;
+        var style = {
+            font: "32px Arial",
+            fill: "#ffffff",
+            align: "center"
+        };
+        this.text = this.add.text(me.game.world.centerX, me.game.world.centerY, "Loading: 0%", style);
+        this.text.anchor.x = 0.5;
+	}
+
+	fileLoaded(progress){
+		this.text.text = "Loading: " + progress + "%";
 	}
 
 }
